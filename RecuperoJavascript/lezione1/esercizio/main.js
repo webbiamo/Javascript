1b1f25947981c6a38f2d4739ef92b9571b7d47b5
webbiamo <pietrodelfino75@gmail.com>
2022-04-07 00:28:37 +0200

    *

--- RecuperoJavascript/lezione1/esercizio/main.js
+++ RecuperoJavascript/lezione1/esercizio/main.js
@@ -25,3 +25,47 @@ switch(mese){
     default:
         console.log('non hai inserito il nome di un mese');  // nessuna delle condizioni precedenti
 }
+
+// //variante esercizio eseguita con ciclo for e if NON FUNZIONANTE E NON COMPLETATA
+
+// var promptMese = prompt('Inserisci il mese').toLowerCase();  // prompt inserimento mese con conversione in minuscolo
+// var mese = promptMese[0].toUpperCase() + promptMese.substring(1) // prima lettera stringa convertita in maiuscolo
+// console.log("Hai inserito il mese di " + mese);  //verifica corretta conversione stringa in console 
+
+
+// const mesi = ["Gennaio", 
+// 			  "Febbraio", 
+//               "Marzo", 
+//               "Aprile", 
+//               "Maggio", 
+//               "Giugno",
+//               "Luglio",
+//               "Agosto",
+//               "Settembre",
+//               "Ottobre",
+//               "Novembre",
+//               "Dicembre"];
+
+// let text = "";
+// //let text1 = "non hai scelto un mese";
+// for (let i = 0; i < mesi.length; i++) {
+//   if (mesi[i] = mese){
+//     console.log (mesi[i] + '<br>');
+//  }else if (mesi[i] = mese){
+//     console.log (mesi[i] + '<br>');
+//  }else if (mesi[i] =  mese){
+//  	console.log (mesi[i] + '<br>');
+// }else if (mesi[i] =  mese){
+//     console.log (mesi[i] + "<br>");
+// }else if (mesi[i] =  mese){
+//     console.log (mesi[i] + "<br>");
+// }else if (mesi[i] =  mese){
+//     console.log (mesi[i] + "<br>");
+// }else if (mesi[i] =  mese){
+//     console.log (mesi[i] + "<br>");
+// }else if (mesi[i] =  mese){
+//     console.log (mesi[i] + "<br>");    
+//  }else{
+//     console.log ("non hai scelto un mese" + "<br>");
+//  }
+//  }
